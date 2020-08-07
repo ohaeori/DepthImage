51d5a9369483b44b06679a5d95f5e0271bd9a20d
ohaeori <0typhoon416@gmail.com>
2020-08-07 22:41:54 +0900

    all

---
+++ unity-depth-camera-master/Assets/DepthCam/HiResScreenShots.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiResScreenShots : MonoBehaviour
+{
+    private int resWidth = 1280;
+    private int resHeight = 752;
+
+    private bool takeHiResShot = false;
+
+    private void Start()
+    {
+        ScreenShot();
+    }
+    void Awake()
+    {
+        resWidth = Screen.width;
+        resHeight = Screen.height;
+    }
+
+    public static string ScreenShotName(int width, int height)
+    {
+        return string.Format("{0}/Image/screen_{1}_depth.png",
+                             Application.dataPath,
+                             //width, height,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void TakeHiResShot()
+    {
+        takeHiResShot = true;
+    }
+
+    void ScreenShot()
+    {
+        RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+        GetComponent<Camera>().targetTexture = rt;
+        Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        GetComponent<Camera>().Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        GetComponent<Camera>().targetTexture = null;
+        RenderTexture.active = null; // JC: added to avoid errors
+        Destroy(rt);
+
+        // 파일로 저장.
+        byte[] bytes = screenShot.EncodeToPNG();
+        string filename = ScreenShotName(resWidth, resHeight);
+        System.IO.File.WriteAllBytes(filename, bytes);
+        Debug.Log(string.Format("Took screenshot to: {0}", filename));
+    }
+}
